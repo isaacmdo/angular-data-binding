b41cb192e53038506604b5da52fd91c82bfbe882
Isaac <isaacestudo1999@gmail.com>
2021-10-24 23:06:39 -0300

    ...

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { TodoListComponent } from './pages/todo/todo-list/todo-list.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { DataBindingComponent } from './pages/data-binding/data-binding.component';
+
+const routes: Routes = [
+  { path: '', component: UsersListComponent},
+  { path: 'form', component: UserFormComponent},
+  { path: 'form/:id', component: UserFormComponent},
+  { path: 'todo', component: TodoListComponent},
+  { path: 'data-binding', component: DataBindingComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
